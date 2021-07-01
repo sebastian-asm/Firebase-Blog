139e255e5aee6a1ee62def9c4174ef0338676c92
Sebastian <sebastian.asm@outlook.com>
2021-07-01 19:30:39 -0300

    renderizando los posts propios del usuario logueado

--- js/firebase/post.js
+++ js/firebase/post.js
@@ -1,5 +1,6 @@
 import alerta from '../ui/alerta.js';
 import cargando from '../ui/cargando.js';
+import gridPosts from '../ui/gridPosts.js';
 import sinPosts from '../ui/sinPosts.js';
 
 const POSTS = 'posts';
@@ -55,44 +56,11 @@ export function obtenerPosts(e) {
         sinPosts('🙃 ¡Anímate! aun no hay post, escribir uno.');
       } else {
         // Eliminando el mensaje sin posts
-        if (document.querySelector('#sin-posts'))
+        if (document.querySelector('#sin-posts')) {
           document.querySelector('#sin-posts').remove();
-
-        const main = document.querySelector('main');
-        const div = document.createElement('div');
-        const h1 = document.createElement('h1');
-        const templatePost = document.querySelector('#template-post').content;
-        const fragment = document.createDocumentFragment();
-
-        div.id = 'grid-posts';
-        div.classList.add('grid-posts');
-        h1.textContent = 'Todos los posts publicados';
-
-        // Limpiando la grid para volver a renderizar con los nuevos posts
-        if (document.querySelector('#grid-posts')) {
-          document.querySelector('#grid-posts').remove();
-          document.querySelector('h1').remove();
         }
 
-        snapshot.forEach((post) => {
-          const { titulo, descripcion, fecha } = post.data();
-          const dia = new Date(fecha).getDate();
-          const mes = new Date(fecha).getMonth() + 1;
-          const anio = new Date(fecha).getFullYear();
-
-          templatePost.querySelector(
-            '#fecha-post'
-          ).textContent = `${dia}/${mes}/${anio}`;
-          templatePost.querySelector('#titulo').textContent = titulo;
-          templatePost.querySelector('#descripcion').textContent =
-            descripcion.substring(0, 250) + '...';
-
-          const clone = templatePost.cloneNode(true);
-          fragment.appendChild(clone);
-        });
-
-        div.appendChild(fragment);
-        main.append(h1, div);
+        gridPosts(snapshot); // Renderizando los posts
       }
 
       // Eliminando el mensaje de cargando
@@ -106,6 +74,8 @@ export function obtenerPostsUsuario(e) {
   e.preventDefault();
   const { uid } = firebase.auth().currentUser;
 
+  cargando();
+
   db.collection(POSTS)
     .where('autor.uid', '==', uid) // Filtrando por el id del usuario logueado
     .onSnapshot((snapshot) => {
@@ -117,9 +87,11 @@ export function obtenerPostsUsuario(e) {
 
         sinPosts('😱 Aun no tienes posts tuyos publicados.');
       } else {
-        snapshot.forEach((post) => {
-          console.log(post.data());
-        });
+        gridPosts(snapshot);
+      }
+
+      if (document.querySelector('#cargando')) {
+        document.querySelector('#cargando').remove();
       }
     });
 }
---
+++ js/ui/gridPosts.js
@@ -0,0 +1,37 @@
+export default function gridPosts(snapshot) {
+  const main = document.querySelector('main');
+  const div = document.createElement('div');
+  const h1 = document.createElement('h1');
+  const templatePost = document.querySelector('#template-post').content;
+  const fragment = document.createDocumentFragment();
+
+  div.id = 'grid-posts';
+  div.classList.add('grid-posts');
+  h1.textContent = 'Todos los posts publicados';
+
+  // Limpiando la grid para volver a renderizar con los nuevos posts
+  if (document.querySelector('#grid-posts')) {
+    document.querySelector('#grid-posts').remove();
+    document.querySelector('h1').remove();
+  }
+
+  snapshot.forEach((post) => {
+    const { titulo, descripcion, fecha } = post.data();
+    const dia = new Date(fecha).getDate();
+    const mes = new Date(fecha).getMonth() + 1;
+    const anio = new Date(fecha).getFullYear();
+
+    templatePost.querySelector(
+      '#fecha-post'
+    ).textContent = `${dia}/${mes}/${anio}`;
+    templatePost.querySelector('#titulo').textContent = titulo;
+    templatePost.querySelector('#descripcion').textContent =
+      descripcion.substring(0, 250) + '...';
+
+    const clone = templatePost.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+
+  div.appendChild(fragment);
+  main.append(h1, div);
+}
